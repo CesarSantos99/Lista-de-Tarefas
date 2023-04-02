2e89eb1df2ad6646429c9cd2e7e97b8c3efd9cf3
César <cesarsantossouza99@gmail.com>
2023-04-02 19:46:02 -0300

    começando

---
+++ src/styles/index.js
@@ -0,0 +1,16 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+        outline: none;
+        background-color: red
+    }
+`;
+
+export const Conatainer = styled.div `
+    di
+`
